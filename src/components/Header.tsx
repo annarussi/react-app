e97a23e91e133f0bccab5aee2b97154b7460d70b
Anna Russi <annarussi@gmail.com>
2023-07-01 17:33:32 +0100

    created the navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,15 @@
+
 export default function Header() {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <>
+      <nav className="nav shadow" >
+        <div className="container d-flex align-items-center m-0">
+          <img src="https://www.leonidas.com/themes/custom/leonidas_theme/logo.jpg" alt="Leonidas Logo" className="leonidas-logo" />
+          <p>Find the your favorite Leonidas Chocolate!</p>
+        </div>
+      </nav>
+
+    </>
 
   )
 }
