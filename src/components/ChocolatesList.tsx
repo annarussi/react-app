c84e6686fd01d2529af5df8794e644225e93dbeb
Anna Russi <annarussi@gmail.com>
2023-07-01 16:30:39 +0100

    added style to search input and conditional for the output

--- src/components/ChocolatesList.tsx
+++ src/components/ChocolatesList.tsx
@@ -1,24 +1,61 @@
-import {chocolates} from '../data/chocolates'
+import { chocolatesIndex } from '../data/chocolates';
+import React from 'react'
+
 
 export default function ChocolatesList() {
+  const [search, setSearch] = React.useState("");
+
+  const searchLowerCase = search.toLowerCase();
+
+  const chocolates = chocolatesIndex.filter(chocolate =>
+    chocolate.name.toLowerCase().includes(searchLowerCase) ||
+    chocolate.type.toLowerCase().includes(searchLowerCase) ||
+    chocolate.filling.toLowerCase().includes(searchLowerCase)
+    );
+
+    const searchInput = <input
+                          className="form-control w-50 m-auto mt-5 mb-5"
+                          type="search"
+                          placeholder='Search for a chocolate'
+                          value={search}
+                          onChange={(event) => setSearch(event.target.value)}/>
+
+// --------------------- What will Display if there are no chocolates matching the search ---------------------
+  if (chocolates.length === 0) {
+    return (
+      <>
+      {searchInput}
+      <h3 className="text-center"><em>Sorry, we couldn't find any chocolates matching your search.</em></h3>
+      </>
+
+    )
+  }
+  // --------------------- What will Display if there are chocolates matching the search ---------------------
+  else {
   return (
-    <div className="chocolates-cards m-auto">
-        {chocolates.map((chocolate) => (
-          <div className="card shadow-sm chocolate-card ">
-            <img
-              src={`https://www.leonidas.com/sites/default/files/styles/product_ts_list/public/2021-03/${chocolate.img}`}
-              alt={`${chocolate.name}`}
-              className="chocolate-img">
-            </img>
+    <>
+      {searchInput}
 
-            <div className="card-body">
-              <h6 className="text-center">{chocolate.name}</h6>
-              {/* <p className="text-center">{chocolate.description}</p> */}
-              {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
+      <div className="chocolates-cards m-auto">
+
+          {chocolates.map((chocolate) => (
+            <div className="card shadow-sm chocolate-card ">
+              <img
+                src={`https://www.leonidas.com/sites/default/files/styles/product_ts_list/public/${chocolate.img}`}
+                alt={`${chocolate.name}`}
+                className="chocolate-img">
+              </img>
+
+              <div className="card-body">
+                <h6 className="text-center">{chocolate.name}</h6>
+                {/* <p className="text-center">{chocolate.description}</p> */}
+                {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
+              </div>
             </div>
-          </div>
 
-        ))}
-    </div>
+          ))}
+      </div>
+    </>
   )
+}
 }
