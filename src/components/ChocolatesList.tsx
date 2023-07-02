68a7e165667163ed12578677b0567c792aa4be87
Anna Russi <annarussi@gmail.com>
2023-07-02 10:16:43 +0100

    changed the conditional for the no matched results - DRY

--- src/components/ChocolatesList.tsx
+++ src/components/ChocolatesList.tsx
@@ -1,4 +1,4 @@
-import { chocolatesIndex } from '../data/chocolates';
+import { chocolatesList } from '../data/chocolates';
 import React from 'react'
 
 
@@ -7,41 +7,27 @@ export default function ChocolatesList() {
 
   const searchLowerCase = search.toLowerCase();
 
-  const chocolates = chocolatesIndex.filter(chocolate =>
+  const chocolates = chocolatesList.filter(chocolate =>
     chocolate.name.toLowerCase().includes(searchLowerCase) ||
     chocolate.type.toLowerCase().includes(searchLowerCase) ||
     chocolate.filling.toLowerCase().includes(searchLowerCase)
     );
 
-    const searchInput = <input
-                          className="form-control w-50 m-auto mt-5 mb-5 search"
-                          type="search"
-                          placeholder='Search for a chocolate'
-                          value={search}
-                          onChange={(event) => setSearch(event.target.value)}/>
-
-// --------------------- What will Display if there are no chocolates matching the search ---------------------
-  if (chocolates.length === 0) {
-    return (
-      <>
-      {searchInput}
-      <h3 className="text-center no-match-text"><em>Sorry, we couldn't find any chocolates matching your search.</em></h3>
-      </>
-
-    )
-  }
-  // --------------------- What will Display if there are chocolates matching the search ---------------------
-  else {
   return (
     <>
-      {searchInput}
+      <input className="form-control w-50 m-auto mt-5 mb-5 search"
+            type="search"
+            placeholder='Search for a chocolate'
+            value={search}
+            onChange={(event) => setSearch(event.target.value)}/>
 
+      { chocolates.length === 0 && <h3 className="text-center no-match-text"><em>Sorry, we couldn't find any chocolates matching your search.</em></h3> }
       <div className="chocolates-cards m-auto">
 
           {chocolates.map((chocolate) => (
             <div className="card chocolate-card ">
               <img
-                src={`https://www.leonidas.com/sites/default/files/styles/product_ts_list/public/${chocolate.img}`}
+                src={`${chocolate.img}`}
                 alt={`${chocolate.name}`}
                 className="chocolate-img">
               </img>
@@ -49,7 +35,6 @@ export default function ChocolatesList() {
               <div className="card-body">
                 <h6 className="text-center">{chocolate.name}</h6>
                 {/* <p className="text-center">{chocolate.description}</p> */}
-                {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
               </div>
             </div>
 
@@ -57,5 +42,4 @@ export default function ChocolatesList() {
       </div>
     </>
   )
-}
 }
