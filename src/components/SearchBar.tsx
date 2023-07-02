d0fc119c75d56dfcc009c2fb09b6fe71b9c59eea
Anna Russi <annarussi@gmail.com>
2023-07-02 12:34:14 +0100

    fixed bug in the App.tsx

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -8,7 +8,6 @@ interface Props {
 export default function SearchBar({ placeholder, onChange }:Props) {
 
   return (
-
     <input className="form-control w-50 m-auto mt-5 mb-5 search"
     type="search"
     placeholder={placeholder}
@@ -17,16 +16,3 @@ export default function SearchBar({ placeholder, onChange }:Props) {
   )
 
 }
-
-
-
-
-
-
-// const [search, setSearch] = React.useState("");
-
-// const searchLowerCase = search.toLowerCase();
-
-// const chocolates = chocolatesList.filter(chocolate =>
-//   chocolate.name.toLowerCase().includes(searchLowerCase)
-//   );
